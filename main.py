882e45c86a64380e29bdec5aab952ba30022abca
ProfTagus <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>
2025-07-28 11:13:35 -0300

    teste5

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print("Olá Mundo")
 print('testando o commit')
 
-#git add .
-#git commit -m "Adicionando o arquivo main.py"
-#git push origin main
+# git add .
+# git commit -m "Adicionando o arquivo main.py"
+# git push origin main
+# git status
